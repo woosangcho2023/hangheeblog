10fa3c1a5952a7b22ce909a26caea4dc38e5facb
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-04-26 03:12:51 +0900

    post 경로로 get 요청이 들어왔을 경우 모든 게시글과 댓글을 반환하는 router 추가

--- routes/comment.js
+++ routes/comment.js
@@ -248,4 +248,25 @@ const author = comment ? comment.createdBy : post.createdBy;
   }
 });
 
+router.get("/posts", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ created_at: -1 });
+    if (posts.length < 1) {
+      return res.status(400).json({ message: "게시글이 없습니다!" });
+    }
+    const allPosts = posts.map((post) => {
+      return {
+        postId: post._id,
+        title: post.title,
+        content: post.content,
+        createdBy: post.createdBy,
+        createdAt: post.created_at,
+      };
+    });
+    return res.json({ data: allPosts });
+  } catch (err) {
+    return res.status(400).json({ message: "데이터 형식이 올바르지 않습니다" });
+  }
+});
+
 module.exports = router;
