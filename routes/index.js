862f1eee1417ea1ffeff4794609b8ec098d3c449
DESKTOP-HLPGO6C\owner <juster0706@gmail.com>
2023-05-03 18:04:29 +0900

    라우터 등록.

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const postsRouter = require("./post");
 const commentsRouter = require("./comment");
+const deleteRouter = require("./delete");
+const loginRouter = require("./login"); 
+const signupRouter = require("./signup"); 
 
 router.use("/posts", postsRouter);
-
 router.use("/posts/:_postId/comments", commentsRouter);
+router.use("/delete", deleteRouter);
+router.use("/login", loginRouter); 
+router.use("/signup", signupRouter); 
 
 module.exports = router;
